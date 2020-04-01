620d1bc11f36af7b695e0ddbe7889a7875bfbd4e
Leandro Costa <lcosta.sp.br@gmail.com>
2020-04-01 17:32:49 -0300

    feature: implement swagger, closes #10

--- config/config.exs
+++ config/config.exs
@@ -29,3 +29,17 @@ config :phoenix, :json_library, Jason
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
+
+config :sherlock_api, :phoenix_swagger,
+  swagger_files: %{
+    "priv/static/swagger.json" => [
+      # phoenix routes will be converted to swagger paths
+      router: SherlockApiWeb.Router,
+      # (optional) endpoint config used to set host, port and https schemes.
+      endpoint: SherlockApiWeb.Endpoint
+    ]
+  }
+
+config :phoenix_swagger, json_library: Jason
+
+config :sherlock_api, SherlockApiWeb.End, url: [host: "localhost"]
--- lib/sherlock_api_web/router.ex
+++ lib/sherlock_api_web/router.ex
@@ -24,4 +24,28 @@ defmodule SherlockApiWeb.Router do
     delete "/account/:uuid", AccountController, :delete
     post "/:workspace_uuid/account", AccountController, :create
   end
+
+  scope "/api/swagger" do
+    forward "/", PhoenixSwagger.Plug.SwaggerUI, otp_app: :sherlock_api, swagger_file: "swagger.json"
+  end
+
+  def swagger_info do
+    %{
+      basePath: "/api",
+      info: %{
+        version: "1.0",
+        title: "Sherlock Api"
+      },
+      tags: [
+        %{
+          name: "Account",
+          description: "Manager account administrators"
+        },
+        %{
+          name: "Restrict Word",
+          description: "Manager words that you need blocked"
+        }
+      ]
+    }
+  end
 end
